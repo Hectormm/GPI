3a32e90a31590d15b4ba7528d94cd4a207aa02f2
Hectormm <hectormm@gmail.com>
2014-05-26 10:20:46 +0200

    Ordenado por carpetas

--- GPI/Proveedores/Proveedores.Designer.cs
+++ GPI/Proveedores/Proveedores.Designer.cs
@@ -129,7 +129,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(512, 373);
+            this.ClientSize = new System.Drawing.Size(512, 362);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.groupBox2);
